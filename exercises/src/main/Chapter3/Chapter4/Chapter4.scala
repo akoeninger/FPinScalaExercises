712825cc9e832b818dbd11fa0c4cdff88a4a8238
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-09-11 11:12:37 -0500

    Exercise 4.1

--- exercises/src/main/Chapter3/Chapter4/Chapter4.scala
+++ exercises/src/main/Chapter3/Chapter4/Chapter4.scala
@@ -2,7 +2,28 @@ package main.Chapter3.Chapter4
 
 
 object Chapter4 {
-  sealed trait Option[+A]
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(v) => Some(f(v))
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      map(f) getOrElse None
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(v) => v
+    }
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+      this map(Some(_)) getOrElse ob
+
+    def filter(f: A => Boolean): Option[A] =
+      if (map(f) getOrElse false) this
+      else None
+  }
+
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
 
