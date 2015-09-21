cd1b02a89ccf2bc3d924f6f63704dc6d3f0956d5
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-09-21 09:05:34 -0500

    Exercise 4.4

--- exercises/src/main/Chapter3/Chapter4/Chapter4.scala
+++ exercises/src/main/Chapter3/Chapter4/Chapter4.scala
@@ -9,7 +9,7 @@ object Chapter4 {
   def Try[A](a: => A): Option[A] =
     try Some(a)
     catch { case e: Exception => None }
-  
+
   sealed trait Option[+A] {
     def mean(xs: Seq[Double]): Option[Double] =
       if (xs.isEmpty) None
@@ -40,7 +40,7 @@ object Chapter4 {
         mean(xs map (x => math.pow(x - m, 2)))
       }
   }
-  
+
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMap { a1 =>
     b map { b1 =>
       f(a1, b1)
@@ -50,7 +50,30 @@ object Chapter4 {
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
 
+  object Option {
+    def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+      a match {
+        case Nil => Some(Nil)
+        case h :: t =>
+          h match {
+            case None => None
+            case Some(v) => sequence(t) map (v :: _)
+          }
+      }
+    }
+  }
+
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case head :: tail => head flatMap { h =>
+      sequence_1(tail).map(h :: _)
+    }
+  }
+
+  def sequence_book[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+
   def main(args: Array[String]) {
-  
+    println(Option.sequence(List(Some(1), Some(2), None, Some(3))))
   }
 }
