14bc1f810d5cdb9d2f78bcf1d2ae9a924275dc9b
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-10-15 08:57:35 -0500

    Chapter 5: Strictness and laziness
    Exercise 5.10
    
    good ol' fibs

--- exercises/src/main/Chapter5/package.scala
+++ exercises/src/main/Chapter5/package.scala
@@ -122,6 +122,11 @@ package object Chapter5 {
 
     def from(n: Int): Stream[Int] =
       cons(n, from(n + 1))
+
+    val fibs: Stream[Int] = {
+      def go(f0: Int, f1: Int): Stream[Int] = cons(f0, go(f1, f0 + f1))
+      go(0, 1)
+    }
   }
 
   def main(args: Array[String]): Unit = {
