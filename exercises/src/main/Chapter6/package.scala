a134b581f35b377f0e10ef1f6a3766844379fc10
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-10-16 14:21:06 -0500

    Chapter 6: Purely functional state
    Exercise 6.6

--- exercises/src/main/Chapter6/package.scala
+++ exercises/src/main/Chapter6/package.scala
@@ -1,78 +1,89 @@
-package main
+package main.Chapter6
 
 import scala.math._
 
-package object Chapter6 {
-  trait RNG {
-    type Rand[+A] = RNG => (A, RNG)
+trait RNG {
+  type Rand[+A] = RNG => (A, RNG)
 
-    def nextInt: (Int, RNG)
+  def nextInt: (Int, RNG)
 
-    val int: Rand[Int] = _.nextInt
+  val int: Rand[Int] = _.nextInt
 
-    def unit[A](a: A): Rand[A] = rng => (a, rng)
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
 
-    def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
-    def nonNegativeInt(rng: RNG): (Int, RNG) = {
-      val (i, newRNG) = rng.nextInt
-      (if (i < 0) -(i + 1) else i, newRNG)
-    }
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, newRNG) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, newRNG)
+  }
 
-    def double(rng: RNG): (Double, RNG) = _double(rng)
+  val double: Rand[Double] = map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 
-    val _double: Rand[Double] = map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng1) = rng.nextInt
+    val (d, rng2) = rng1.double(rng1)
 
-    def intDouble(rng: RNG): ((Int, Double), RNG) = {
-      val (i, rng1) = rng.nextInt
-      val (d, rng2) = rng1.double(rng1)
+    ((i, d), rng2)
+  }
 
-      ((i, d), rng2)
-    }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), rng2) = intDouble(rng)
 
-    def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-      val ((i, d), rng2) = intDouble(rng)
+    ((d, i), rng2)
+  }
 
-      ((d, i), rng2)
-    }
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
 
-    def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-      val (d1, r1) = double(rng)
-      val (d2, r2) = double(r1)
-      val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 
-      ((d1, d2, d3), r3)
-    }
-
-    def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-      def go(c: Int, r: RNG, list: List[Int]): (List[Int], RNG) = {
-        if (c == 0)
-          (list, r)
-        else {
-          val (i, r1) = r.nextInt
-          go(c - 1, r1, i :: list)
-        }
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(c: Int, r: RNG, list: List[Int]): (List[Int], RNG) = {
+      if (c == 0)
+        (list, r)
+      else {
+        val (i, r1) = r.nextInt
+        go(c - 1, r1, i :: list)
       }
-
-      go(count, rng, Nil)
     }
+
+    go(count, rng, Nil)
   }
 
-  case class SimpleRNG(seed: Long) extends RNG {
-    override def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-      val nextRNG = SimpleRNG(newSeed)
-      val n = (newSeed >>> 16).toInt
-      (n, nextRNG)
-    }
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, r1) = ra(rng)
+    val (b, r2) = rb(r1)
+    (f(a, b), r2)
   }
 
-  def main(args: Array[String]) {
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
 
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+package object Chapter6 {
+
+
+  def main(args: Array[String]): Unit = {
+    println(SimpleRNG(1L).double(SimpleRNG(1L)))
   }
 }
