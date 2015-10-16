5844a0bdd2df1a17e4d3e7843eee9467223746bd
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-10-16 09:50:56 -0500

    Chapter 5: Strictness and laziness
    Exercise 5.16

--- exercises/src/main/Chapter5/package.scala
+++ exercises/src/main/Chapter5/package.scala
@@ -142,8 +142,14 @@ package object Chapter5 {
 
 
     def hasSubsequence[A1 >: A](s: Stream[A1]): Boolean =
-      tails.exists( _ startsWith s)
+      tails.exists(_ startsWith s)
 
+    // unfold goes left to right so must use foldright
+    def scanRight[B](z: => B)(f: (A, => B) => B): Stream[B] = foldRight((z, Stream(z)))((a, p0) => {
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
 
   }
 
