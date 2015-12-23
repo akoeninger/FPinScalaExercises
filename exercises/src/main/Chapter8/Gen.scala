7b9f89be970e7e9939fe4a030867c0e6520f361c
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-12-23 14:17:09 -0600

    Chapter 8: Property-based testing
    8.8
    Factored out a chooser primative that chooses between 2 Gens based on a Gen[bool] result

--- exercises/src/main/Chapter8/Gen.scala
+++ exercises/src/main/Chapter8/Gen.scala
@@ -28,16 +28,46 @@ shell, which you can fill in and modify while working through the chapter.
   * 3) max of list == max of list reverse
   * 4) max of list that includes Int.MaxValue == Int.MaxValue
   */
-trait Prop {
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+
+case class Prop(run: (TestCases, RNG) => Result) {
   def &&(p: Prop): Prop = ???
 }
 
 object Prop {
   type SuccessCount = Int
   type FailedCase = String
+  type TestCases = Int
 
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+  case object Passed extends Result {
+    def isFalsified: Boolean = false
+  }
+  case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+    def isFalsified: Boolean = true
+  }
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
+
+  def randomStream[A](g: Gen[A])(rng: RNG) = Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): FailedCase =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n) map { case (a, i) =>
+      try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch {
+        case e: Exception => Falsified(buildMsg(a, e), i)
+      }
+    } find(_.isFalsified) getOrElse Passed
+  }
+
+
 }
 
 case class Gen[+A](sample: State[RNG, A]) {
