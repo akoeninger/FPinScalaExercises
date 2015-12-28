8503832467892fd5f73551a19157d4aa5c51368a
Adam Koeninger <adam.koeninger@wicourts.gov>
2015-12-28 15:09:23 -0600

    Chapter 8: Property-based testing
    8.14 Helped from book

--- exercises/src/main/Chapter8/Gen.scala
+++ exercises/src/main/Chapter8/Gen.scala
@@ -157,6 +157,13 @@ object Gen {
     !ns.exists(_ > max)
   }
 
+  // We specify that every sorted list is either empty, has one element,
+  // or has no two consecutive elements `(a,b)` such that `a` is greater than `b`.
+  val sortedProp = forAll(listOf(smallInt)) { l =>
+    val ls = l.sorted
+    l.isEmpty || ls.tail.isEmpty || !ls.zip(ls.tail).exists { case (a,b) => a > b }
+  }
+
 }
 
 case class SGen[+A](forSize: Int => Gen[A]) {
