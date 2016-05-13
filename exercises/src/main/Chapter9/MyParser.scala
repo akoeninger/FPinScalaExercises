41d9724c3be59ef1b218c9d39141fa6d83310d09
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-05-13 10:31:48 -0500

    Exercise 9.13
    implement string

---
+++ exercises/src/main/Chapter9/MyParser.scala
@@ -0,0 +1,20 @@
+package main.Chapter9
+import MyParserTypes._
+
+object MyParserTypes {
+  type Parser[+A] = Location => Result[A]
+
+  sealed trait Result[+A]
+  case class Success[+A](get: A, charsConsumed: Int) extends Result[A]
+  case class Failure(get: ParseError) extends Result[Nothing]
+
+}
+
+object MyParsers extends Parsers[Parser] {
+  def string(s: String): Parser[String] = (loc: Location) =>
+    if (loc.input.startsWith(s, loc.offset))
+      Success(s, s.length)
+    else
+      Failure(loc.toError((s"Expected: $s")))
+  
+}
