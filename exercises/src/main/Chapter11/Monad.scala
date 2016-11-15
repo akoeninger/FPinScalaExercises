ef434d9c84b3d8a371eef846d9a70ed576c7b412
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-11-15 16:39:34 -0600

    Exercise 11.4
    Nice use of sequence

--- exercises/src/main/Chapter11/Monad.scala
+++ exercises/src/main/Chapter11/Monad.scala
@@ -45,7 +45,8 @@ trait Monad[M[_]] extends Functor[M] {
   def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] =
     sequence(la map f)
 
-  def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
+  def replicateM[A](n: Int, ma: M[A]): M[List[A]] =
+    sequence(List.fill(n)(ma))
 
   def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] = ???
 
