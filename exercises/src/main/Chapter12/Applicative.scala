b3ee2adc12a6d063b20192e06a412099c135ebbe
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-11-30 15:57:15 -0600

    Chapter 12 .18

--- exercises/src/main/Chapter12/Applicative.scala
+++ exercises/src/main/Chapter12/Applicative.scala
@@ -216,7 +216,9 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
     mapAccum(fa, z)((a, s) => ((), f(s, a)))._2
 
   def fuse[G[_],H[_],A,B](fa: F[A])(f: A => G[B], g: A => H[B])
-                         (implicit G: Applicative[G], H: Applicative[H]): (G[F[B]], H[F[B]]) = ???
+                         (implicit G: Applicative[G], H: Applicative[H]): (G[F[B]], H[F[B]]) = {
+    traverse[({type f[x] = (G[x], H[x])})#f, A, B](fa)(a => (f(a), g(a)))(G product H)
+  }
 
   def compose[G[_]](implicit G: Traverse[G]): Traverse[({type f[x] = F[G[x]]})#f] = ???
 }
