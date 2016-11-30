b1798ed6b29f45325ef2fbb955b38a5e43ff0537
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-11-30 09:16:07 -0600

    Chapter 12 .14
    Used the Id monad

--- exercises/src/main/Chapter12/Applicative.scala
+++ exercises/src/main/Chapter12/Applicative.scala
@@ -173,12 +173,21 @@ object Applicative {
 }
 
 trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
-  def traverse[G[_]:Applicative,A,B](fa: F[A])(f: A => G[B]): G[F[B]] =
+  def traverse[G[_],A,B](fa: F[A])(f: A => G[B])(implicit G: Applicative[G]): G[F[B]] =
     sequence(map(fa)(f))
-  def sequence[G[_]:Applicative,A](fma: F[G[A]]): G[F[A]] =
+
+  def sequence[G[_],A](fma: F[G[A]])(implicit G: Applicative[G]): G[F[A]] =
     traverse(fma)(ma => ma)
 
-  def map[A,B](fa: F[A])(f: A => B): F[B] = ???
+  type Id[A] = A
+
+  val idMonad = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = a
+
+    override def flatMap[A, B](ma: A)(f: (A) => B): B = f(ma)
+  }
+
+  def map[A,B](fa: F[A])(f: A => B): F[B] = traverse[Id, A, B](fa)(f)(idMonad)
 
   import Applicative._
 
@@ -190,11 +199,11 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
     traverse[({type f[x] = State[S,x]})#f,A,B](fa)(f)(Monad.stateMonad)
 
   def mapAccum[S,A,B](fa: F[A], s: S)(f: (A, S) => (B, S)): (F[B], S) =
-    traverseS(fa)((a: A) => (for {
+    traverseS(fa)((a: A) => for {
       s1 <- get[S]
       (b, s2) = f(a, s1)
       _  <- set(s2)
-    } yield b)).run(s)
+    } yield b).run(s)
 
   override def toList[A](fa: F[A]): List[A] =
     mapAccum(fa, List[A]())((a, s) => ((), a :: s))._2.reverse
