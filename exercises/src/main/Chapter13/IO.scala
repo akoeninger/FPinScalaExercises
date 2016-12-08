51d7c6dfbbc041b724016648abf90e6b966f2fac
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-12-08 08:40:17 -0600

    Example of application with pureMain method

--- exercises/src/main/Chapter13/IO.scala
+++ exercises/src/main/Chapter13/IO.scala
@@ -524,6 +524,13 @@ object IO3 {
   abstract class App {
     import java.util.concurrent._
 
-    def unsa
+    def unsafePerformIO[A](a: IO[A])(pool: ExecutorService): A = Par.run(pool)(run(a)(parMonad))
+
+    def main(args: Array[String]): Unit = {
+      val pool = Executors.newFixedThreadPool(8)
+      unsafePerformIO(pureMain(args))(pool)
+    }
+
+    def pureMain(args: IndexedSeq[String]): IO[Unit] // PureMain has no side effects, only effects
   }
 }
