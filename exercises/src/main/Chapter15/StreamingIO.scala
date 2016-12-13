378c68f04587a02f03400b5ffe5b22e31260f39d
Adam Koeninger <adam.koeninger@wicourts.gov>
2016-12-13 08:27:38 -0600

    Chapter 15.2: Simple stream transducers (or processors)

--- exercises/src/main/Chapter15/StreamingIO.scala
+++ exercises/src/main/Chapter15/StreamingIO.scala
@@ -35,11 +35,25 @@ and we could assemble functionality using all the `Stream` functions
 we know and love.
    */
 
-object Examples {
-val lines: Stream[String] = sys.error("defined elsewhere")
-val ex1 = lines.zipWithIndex.exists(_._2 + 1 >= 40000)
-val ex2 = lines.filter(!_.trim.isEmpty).zipWithIndex.exists(_._2 + 1 >= 40000)
-val ex3 = lines.take(40000).map(_.head).indexOfSlice("abracadabra".toList)
+  object Examples {
+    val lines: Stream[String] = sys.error("defined elsewhere")
+    val ex1 = lines.zipWithIndex.exists(_._2 + 1 >= 40000)
+    val ex2 = lines.filter(!_.trim.isEmpty).zipWithIndex.exists(_._2 + 1 >= 40000)
+    val ex3 = lines.take(40000).map(_.head).indexOfSlice("abracadabra".toList)
+  }
+
+  /* Cheat to return IO[Stream[String]] to represent the lines of the file. "Lazy I/O" */
+  def lines(filename: String): IO[Stream[String]] = IO {
+    val src = io.Source.fromFile(filename)
+    src.getLines().toStream append { src.close(); Stream.empty }
+  }
 }
 
+object SimpleStreamTransducers {
+  /*
+    We now introduce a type, `Process`, representing pure, single-input
+    stream transducers. It can be in of three states - it can be
+    emitting a value to the output (`Emit`), reading a value from its
+    input (`Await`) or signaling termination via `Halt`.
+                               */
 }
